5a4fead7824228f88db03036c8887f137a662006
Christine Hager <cehager@gmail.com>
2019-01-03 13:09:16 -0800

    Installs contentful, and hooks into api, updates gitignore so accessToken and spaceID not tracked

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,30 @@ import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
 
+import config from './config';
+
+const contentful = require('contentful');
+
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app');
+const client = contentful.createClient({
+  space: config.mySpaceId,
+  accessToken: config.myAccessToken,
+});
+
+let recipes = {};
+
+client.getEntries({})
+  .then((response: {}) => {
+    recipes = response;
+    new Vue({
+      router,
+      render: h => h(App),
+      data: {
+        recipes,
+      },
+    }).$mount('#app');
+    console.log(recipes);
+  })
+  // eslint-disable-next-line
+  .catch(console.error);
