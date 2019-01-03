73aad763338e557810a51802f6a8f0787f5481e5
Christine Hager <cehager@gmail.com>
2019-01-03 12:33:07 -0800

    Update to vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,12 @@
 <template>
   <div id="app">
-    <RecipeList msg="Welcome to Your Vue.js App"/>
-    <p v-for="(recipe, index) in recipes" :key="index">{{ recipe.fields.title }}</p>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import RecipeList from './components/RecipeList.vue';
-
-export default {
-  name: 'app',
-  components: {
-    RecipeList,
-  },
-  data() {
-    const recipes = this.$root.recipes.items;
-    return { recipes };
-  },
-};
-</script>
 
 <style lang="scss">
 #app {
@@ -27,6 +15,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
